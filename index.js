53981fb63d7a4f429dae2e6a1057a53a6fd08bf1
Moritz Wick <moritzwick@Moritzs-Mac-mini-2.local>
2017-11-03 12:31:03 +0100

    added storyboard support

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var fs     = require('fs-extra');
-var path   = require('path');
+var fs = require('fs-extra');
+var path = require('path');
 var xml2js = require('xml2js');
-var ig     = require('imagemagick');
+var ig = require('imagemagick');
 var colors = require('colors');
-var _      = require('underscore');
-var Q      = require('q');
-var argv   = require('minimist')(process.argv.slice(2));
+var _ = require('underscore');
+var Q = require('q');
+var argv = require('minimist')(process.argv.slice(2));
 
 /**
  * @var {Object} settings - names of the config file and of the splash image
@@ -14,11 +14,13 @@ var settings = {};
 settings.CONFIG_FILE = argv.config || 'config.xml';
 settings.SPLASH_FILE = argv.splash || 'splash.png';
 settings.OLD_XCODE_PATH = argv['xcode-old'] || false;
+settings.STORYBOARD = argv['use-storyboard'] || false;
 
 /**
  * Check which platforms are added to the project and return their splash screen names and sizes
  *
  * @param  {String} projectName
+ * @var {Object} console utils
  * @return {Promise} resolves with an array of platforms
  */
 var getPlatforms = function (projectName) {
@@ -28,70 +30,101 @@ var getPlatforms = function (projectName) {
 
   if (settings.OLD_XCODE_PATH) {
     xcodeFolder = '/Resources/splash/';
+  } else if (settings.STORYBOARD) {
+    xcodeFolder = '/Images.xcassets/LaunchStoryboard.imageset/'
+  }
+
+  var splashImages = [
+    // iPhone
+    { name: 'Default~iphone.png', width: 320, height: 480 },
+    { name: 'Default@2x~iphone.png', width: 640, height: 960 },
+    { name: 'Default-568h@2x~iphone.png', width: 640, height: 1136 },
+    { name: 'Default-667h.png', width: 750, height: 1334 },
+    { name: 'Default-736h.png', width: 1242, height: 2208 },
+    { name: 'Default-Landscape-736h.png', width: 2208, height: 1242 },
+    // { name: 'Default-2436h.png', width: 1125, height: 2436 },
+    // { name: 'Default-Landscape-2436h.png', width: 2436, height: 1125 },
+    // iPad
+    { name: 'Default-Portrait~ipad.png', width: 768, height: 1024 },
+    { name: 'Default-Portrait@2x~ipad.png', width: 1536, height: 2048 },
+    { name: 'Default-Landscape~ipad.png', width: 1024, height: 768 },
+    { name: 'Default-Landscape@2x~ipad.png', width: 2048, height: 1536 }
+  ]
+
+  if (settings.STORYBOARD) {
+    splashImages = [
+      { name: 'Default@2x~universal~anyany.png', width: 1334, height: 1334 },
+      { name: 'Default@2x~universal~comany.png', width: 750, height: 1334 },
+      { name: 'Default@2x~universal~comcom.png', width: 1334, height: 750 },
+      { name: 'Default@3x~universal~anyany.png', width: 2208, height: 2208 },
+      { name: 'Default@3x~universal~anycom.png', width: 2208, height: 1242 },
+      { name: 'Default@3x~universal~comany.png', width: 1242, height: 2208 },
+    ]
   }
 
   platforms.push({
-    name : 'ios',
+    name: 'ios',
     // TODO: use async fs.exists
-    isAdded : fs.existsSync('platforms/ios'),
-    splashPath : 'platforms/ios/' + projectName + xcodeFolder,
-    splash : [
-      // iPhone
-      { name: 'Default~iphone.png',            width: 320,  height: 480  },
-      { name: 'Default@2x~iphone.png',         width: 640,  height: 960  },
-      { name: 'Default-568h@2x~iphone.png',    width: 640,  height: 1136 },
-      { name: 'Default-667h.png',              width: 750,  height: 1334 },
-      { name: 'Default-736h.png',              width: 1242, height: 2208 },
-      { name: 'Default-Landscape-736h.png',    width: 2208, height: 1242 },
-      { name: 'Default-2436h.png',             width: 1125, height: 2436 },
-      { name: 'Default-Landscape-2436h.png',   width: 2436, height: 1125 },
-      // iPad
-      { name: 'Default-Portrait~ipad.png',     width: 768,  height: 1024 },
-      { name: 'Default-Portrait@2x~ipad.png',  width: 1536, height: 2048 },
-      { name: 'Default-Landscape~ipad.png',    width: 1024, height: 768  },
-      { name: 'Default-Landscape@2x~ipad.png', width: 2048, height: 1536 }
-    ]
+    isAdded: fs.existsSync('platforms/ios'),
+    splashPath: 'platforms/ios/' + projectName + xcodeFolder,
+    splash: splashImages
   });
-  platforms.push({
-    name : 'android',
-    isAdded : fs.existsSync('platforms/android'),
-    splashPath : 'platforms/android/res/',
-    splash : [
-      // Landscape
-      { name: 'drawable-land-ldpi/screen.png',  width: 320,  height: 200  },
-      { name: 'drawable-land-mdpi/screen.png',  width: 480,  height: 320  },
-      { name: 'drawable-land-hdpi/screen.png',  width: 800,  height: 480  },
-      { name: 'drawable-land-xhdpi/screen.png', width: 1280, height: 720  },
-      { name: 'drawable-land-xxhdpi/screen.png', width: 1600, height: 960  },
-      { name: 'drawable-land-xxxhdpi/screen.png', width: 1920, height: 1280  },
-      // Portrait
-      { name: 'drawable-port-ldpi/screen.png',  width: 200,  height: 320  },
-      { name: 'drawable-port-mdpi/screen.png',  width: 320,  height: 480  },
-      { name: 'drawable-port-hdpi/screen.png',  width: 480,  height: 800  },
-      { name: 'drawable-port-xhdpi/screen.png', width: 720,  height: 1280 },
-      { name: 'drawable-port-xxhdpi/screen.png', width: 960, height: 1600  },
-      { name: 'drawable-port-xxxhdpi/screen.png', width: 1280, height: 1920  }
-    ]
-  });
-  platforms.push({
-    name : 'windows',
-    isAdded : fs.existsSync('platforms/windows'),
-    splashPath : 'platforms/windows/images/',
-    splash : [
-      // Landscape
-      { name: 'SplashScreen.scale-100.png', width: 620,  height: 300  },
-      { name: 'SplashScreen.scale-125.png', width: 775,  height: 375  },
-      { name: 'SplashScreen.scale-140.png', width: 868,  height: 420  },
-      { name: 'SplashScreen.scale-150.png', width: 930,  height: 450  },
-      { name: 'SplashScreen.scale-180.png', width: 1116,  height: 540  },
-      { name: 'SplashScreen.scale-200.png', width: 1240, height: 600  },
-      { name: 'SplashScreen.scale-400.png', width: 2480, height: 1200 },
-      // Portrait
-      { name: 'SplashScreenPhone.scale-240.png', width: 1152,  height: 1920  },
-      { name: 'SplashScreenPhone.scale-140.png', width: 672,  height: 1120  },
-      { name: 'SplashScreenPhone.scale-100.png', width: 480,  height: 800  }
-    ]
-  });
+
+  if (!settings.STORYBOARD) {
+    platforms.push({
+      name: 'android',
+      isAdded: fs.existsSync('platforms/android'),
+      splashPath: 'platforms/android/res/',
+      splash: [
+        // Landscape
+        { name: 'drawable-land-ldpi/screen.png', width: 320, height: 200 },
+        { name: 'drawable-land-mdpi/screen.png', width: 480, height: 320 },
+        { name: 'drawable-land-hdpi/screen.png', width: 800, height: 480 },
+        { name: 'drawable-land-xhdpi/screen.png', width: 1280, height: 720 },
+        { name: 'drawable-land-xxhdpi/screen.png', width: 1600, height: 960 },
+        { name: 'drawable-land-xxxhdpi/screen.png', width: 1920, height: 1280 },
+        // Portrait
+        { name: 'drawable-port-ldpi/screen.png', width: 200, height: 320 },
+        { name: 'drawable-port-mdpi/screen.png', width: 320, height: 480 },
+        { name: 'drawable-port-hdpi/screen.png', width: 480, height: 800 },
+        { name: 'drawable-port-xhdpi/screen.png', width: 720, height: 1280 },
+        { name: 'drawable-port-xxhdpi/screen.png', width: 960, height: 1600 },
+        { name: 'drawable-port-xxxhdpi/screen.png', width: 1280, height: 1920 }
+      ]
+    });
+    platforms.push({
+      name: 'windows',
+      isAdded: fs.existsSync('platforms/windows'),
+      splashPath: 'platforms/windows/images/',
+      splash: [
+        // Landscape
+        { name: 'SplashScreen.scale-100.png', width: 620, height: 300 },
+        { name: 'SplashScreen.scale-125.png', width: 775, height: 375 },
+        { name: 'SplashScreen.scale-140.png', width: 868, height: 420 },
+        { name: 'SplashScreen.scale-150.png', width: 930, height: 450 },
+        { name: 'SplashScreen.scale-180.png', width: 1116, height: 540 },
+        { name: 'SplashScreen.scale-200.png', width: 1240, height: 600 },
+        { name: 'SplashScreen.scale-400.png', width: 2480, height: 1200 },
+        // Portrait
+        { name: 'SplashScreenPhone.scale-240.png', width: 1152, height: 1920 },
+        { name: 'SplashScreenPhone.scale-140.png', width: 672, height: 1120 },
+        { name: 'SplashScreenPhone.scale-100.png', width: 480, height: 800 }
+      ]
+    });
+  }
+
+  if (settings.STORYBOARD) {
+    if (projectName) {
+      fs.copyFile(path.join(__dirname + '/storyboardfiles/Contents.json'), 'platforms/ios/' + projectName + xcodeFolder + 'Contents.json', (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Contents file copied');
+        }
+      });
+    }
+  }
+
   deferred.resolve(platforms);
   return deferred.promise;
 };
@@ -163,7 +196,7 @@ var generateSplash = function (platform, splash) {
     format: 'png',
     width: splash.width,
     height: splash.height
-  } , function(err, stdout, stderr){
+  }, function (err, stdout, stderr) {
     if (err) {
       deferred.reject(err);
     } else {
@@ -206,7 +239,7 @@ var generateSplashes = function (platforms) {
   var deferred = Q.defer();
   var sequence = Q();
   var all = [];
-  _(platforms).where({ isAdded : true }).forEach(function (platform) {
+  _(platforms).where({ isAdded: true }).forEach(function (platform) {
     sequence = sequence.then(function () {
       return generateSplashForPlatform(platform);
     });
@@ -226,7 +259,7 @@ var generateSplashes = function (platforms) {
 var atLeastOnePlatformFound = function () {
   var deferred = Q.defer();
   getPlatforms().then(function (platforms) {
-    var activePlatforms = _(platforms).where({ isAdded : true });
+    var activePlatforms = _(platforms).where({ isAdded: true });
     if (activePlatforms.length > 0) {
       display.success('platforms found: ' + _(activePlatforms).pluck('name').join(', '));
       deferred.resolve();
@@ -283,15 +316,15 @@ var configFileExists = function () {
 display.header('Checking Project & Splash');
 
 atLeastOnePlatformFound()
-.then(validSplashExists)
-.then(configFileExists)
-.then(getProjectName)
-.then(getPlatforms)
-.then(generateSplashes)
-.catch(function (err) {
-  if (err) {
-    console.log(err);
-  }
-}).then(function () {
-  console.log('');
-});
+  .then(validSplashExists)
+  .then(configFileExists)
+  .then(getProjectName)
+  .then(getPlatforms)
+  .then(generateSplashes)
+  .catch(function (err) {
+    if (err) {
+      console.log(err);
+    }
+  }).then(function () {
+    console.log('');
+  });
